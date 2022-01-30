58095d94fefd366c4bd6f8e1b5d9f8622926e5b4
jiwon199 <jiwonchoi99@naver.com>
2022-01-31 02:00:17 +0900

    lunchtray

---
+++ lunch-tray/app/build/generated/source/kapt/debug/com/example/lunchtray/BR.java
@@ -0,0 +1,15 @@
+package com.example.lunchtray;
+
+public class BR {
+  public static final int AccompanimentMenuFragment = 1;
+
+  public static final int CheckoutFragment = 2;
+
+  public static final int EntreeMenuFragment = 3;
+
+  public static final int SideMenuFragment = 4;
+
+  public static final int _all = 0;
+
+  public static final int viewModel = 5;
+}
